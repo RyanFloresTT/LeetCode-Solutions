fb671b57ce55d6a4fb5376d54e03f3464b40144d
Ryan Flores <53247675+RyanFloresTT@users.noreply.github.com>
2023-09-05 10:48:46 -0700

    Time: 134 ms (59.18%), Space: 43.9 MB (11.87%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public int[] Intersect(int[] nums1, int[] nums2) {
+        
+        List<int> result = new();
+        
+        Dictionary<int, int> num1Dict = CountArrayToDict(nums1);
+        Dictionary<int, int> num2Dict = CountArrayToDict(nums2);
+        Dictionary<int, int> smallDict = num1Dict.Count <= num2Dict.Count ? num1Dict : num2Dict;
+        
+        foreach (var kvp in smallDict){
+            if (num1Dict.ContainsKey(kvp.Key) && num2Dict.ContainsKey(kvp.Key)){
+                int smallKey = num1Dict[kvp.Key] <= num2Dict[kvp.Key] ? num1Dict[kvp.Key] : num2Dict[kvp.Key];
+                for (int i = 0; i < smallKey; i++){
+                    result.Add(kvp.Key);
+                }
+            }
+        }
+        
+        return result.ToArray();
+    }
+    
+    public Dictionary<int, int> CountArrayToDict(int[] arr){
+        Dictionary<int, int> res = new();
+        
+        for (int i = 0; i < arr.Length; i++){
+            if (!res.ContainsKey(arr[i])){
+                res.Add(arr[i], 1);
+            } else {
+                res[arr[i]] += 1;
+            }
+        }
+        
+        return res;
+    }
+}
